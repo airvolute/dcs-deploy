560c6f7de655e3fbc781e6ef7c6893ede6aac689
Michal <michal.bachraty@gmail.com>
2023-11-13 16:37:13 +0100

    update dcs_first_boot script to work properly
    Signed-off-by: Michal <michal.bachraty@gmail.com>

--- local/overlays/dcs_first_boot/apply_dcs_first_boot.sh
+++ local/overlays/dcs_first_boot/apply_dcs_first_boot.sh
@@ -19,7 +19,7 @@ echo "script_path: $script_path"
 
 resources_path=${2:-$script_path/resources}
 echo "resouce path: $resources_path"
-exit 0
+
 # create first boot file
 first_boot_file_path=${L4T_rootfs_path}/etc/first_boot
 sudo touch first_boot_file_path
@@ -32,21 +32,21 @@ bin_destination=${L4T_rootfs_path}/usr/local/bin
 uhubctl_destination=${L4T_rootfs_path}/home/dcs_user
 
 # USB3_CONTROL service
-sudo cp ${resources_path}/usb3_control/usb3_control.service ${service_destination}
-sudo cp ${resources_path}/usb3_control/usb3_control.sh ${bin_destination}
+sudo cp ${resources_path}/usb3_control/usb3_control.service ${service_destination}/
+sudo cp ${resources_path}/usb3_control/usb3_control.sh ${bin_destination}/
 sudo chmod +x ${bin_destination}/usb3_control.sh
 
 # USB_HUB_CONTROL service
-sudo cp ${resources_path}/usb_hub_control/usb_hub_control.service ${service_destination}
-sudo cp ${resources_path}/usb_hub_control/usb_hub_control.sh ${bin_destination}
+sudo cp ${resources_path}/usb_hub_control/usb_hub_control.service ${service_destination}/
+sudo cp ${resources_path}/usb_hub_control/usb_hub_control.sh ${bin_destination}/
 sudo chmod +x ${bin_destination}/usb_hub_control.sh
 
 # uhubctl
-sudo cp ${resources_path}/uhubctl_2.1.0-1_arm64.deb ${uhubctl_destination}
+sudo cp ${resources_path}/uhubctl_2.1.0-1_arm64.deb ${uhubctl_destination}/
 
 # FIRST_BOOT service
-sudo cp ${resources_path}/dcs_first_boot.service ${service_destination}
-sudo cp ${resources_path}/dcs_first_boot.sh ${bin_destination}
+sudo cp ${resources_path}/dcs_first_boot.service ${service_destination}/
+sudo cp ${resources_path}/dcs_first_boot.sh ${bin_destination}/
 sudo chmod +x ${bin_destination}/dcs_first_boot.sh
 
-sudo ln -s /etc/systemd/system/dcs_first_boot.service ${service_destination}/multi-user.target.wants/dcs_first_boot.service
+sudo ln -sf /etc/systemd/system/dcs_first_boot.service ${service_destination}/multi-user.target.wants/dcs_first_boot.service
