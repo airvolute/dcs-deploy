9855cefa8e1782cadfa6d77c87a99568ced06a52
martin.sedlacek <martin.sedlacek@airvolute.com>
2024-03-05 09:01:40 +0100

    Corrected ethernet switch reset.

--- resources/usb_hub_control/usb_hub_control.sh
+++ resources/usb_hub_control/usb_hub_control.sh
@@ -4,8 +4,9 @@ device=$(cat /sys/devices/soc0/soc_id)
 if [[ "$device" == "35" ]]; then
     echo 331 > /sys/class/gpio/export
     echo out > /sys/class/gpio/PCC.03/direction
+    echo 0 > /sys/class/gpio/PCC.03/value
     echo 1 > /sys/class/gpio/PCC.03/value
-    sleep 1
+    
     i2cset -y 0 0x2d 0xAA 0x55 0x00 i
     sleep 1
 fi
