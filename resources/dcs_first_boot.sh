547b2cb3f8c225e42a8334787ca7a860896b4070
Eduard Mraz <29916860+eMrazSVK@users.noreply.github.com>
2024-05-15 10:24:32 +0200

    Add correct permissions and UDEV rules

--- resources/dcs_first_boot.sh
+++ resources/dcs_first_boot.sh
@@ -5,6 +5,19 @@ sudo systemctl enable fan_control.service
 sudo systemctl start fan_control.service
 echo "Fan control service enabled and started"
 
+# Set correct permissions and udev rules
+if [ ! -f /etc/udev/rules.d/61-jetson-common.rules ] ; then
+    sudo touch /etc/udev/rules.d/61-jetson-common.rules
+    echo 'KERNEL=="gpiochip*", SUBSYSTEM=="gpio", MODE="0660", GROUP="gpio"' | sudo tee -a /etc/udev/rules.d/61-jetson-common.rules
+    echo 'KERNEL=="i2c*", SUBSYSTEM=="i2c", MODE="0660", GROUP="i2c"' | sudo tee -a /etc/udev/rules.d/61-jetson-common.rules
+    echo 'KERNEL=="/dev/ttyTHS0*", SUBSYSTEM=="dialout", MODE="0660", GROUP="dialout"' | sudo tee -a /etc/udev/rules.d/61-jetson-common.rules
+fi
+
+sudo usermod -a -G i2c dcs_user
+sudo usermod -a -G gpio dcs_user
+sudo usermod -a -G dialout dcs_user
+sudo udevadm control --reload-rules && udevadm trigger
+
 # Enable start usb3_control service after start-up
 sudo systemctl enable usb3_control.service
 echo "Service enabled"
